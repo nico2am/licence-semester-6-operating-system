26b52b195be3e7e383a497ee4c776176529d523b
Nicolas Bellni <bellini.nicolas@ymail.com>
2016-03-21 17:06:05 +0100

    TP5 Fini

--- TP5/thread.c
+++ TP5/thread.c
@@ -1,33 +1,56 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <pthread.h> 
+#include <pthread.h>
+#include <semaphore.h> 
+#include <unistd.h>
 
-volatile char theChar = '\0';
-volatile char afficher = 0;
+#define TAILLE_TAMPON 10
+
+volatile char tampon[TAILLE_TAMPON];
+volatile int ptr_entree = 0;
+volatile int ptr_sorti = 0;
+
+sem_t n_plein;
+sem_t n_vide;
 
 void* lire(void* name) {
+    (void) name;
     do {
-        while (afficher == 1); /* attendre mon tour */
-        theChar = getchar();
-        afficher = 1; /* donner le tour */
-        sched_yield();
-    } while (theChar != 'F');
+        while (1) {
+            //sleep(1);
+            sem_wait(&n_vide);
+            tampon[ptr_entree] = getchar();
+            ptr_entree++;
+            ptr_entree %= TAILLE_TAMPON;
+            sched_yield();
+            sem_post(&n_plein);
+        }
+    } while (tampon[ptr_entree] != 'F');
     return NULL;
 }
 
 void* affichage(void* name) {
-    int cpt = 0;
+    (void) name;
     do {
-        while (afficher == 0) cpt++; /* attendre */
-        printf("cpt = %d, car = %c\n", cpt, theChar);
-        afficher = 0; /* donner le tour */
-        sched_yield();
-    } while (theChar != 'F');
+        while (1) {
+            sleep(1);
+            sem_wait(&n_plein);
+            printf("car = %c\n", tampon[ptr_sorti]);
+            ptr_sorti++;
+            ptr_sorti %= TAILLE_TAMPON;
+            
+            sched_yield();
+            sem_post(&n_vide);
+        }
+    } while (tampon[ptr_sorti] != 'F');
     return NULL;
 }
 
 int main(void) {
     pthread_t filsA, filsB;
+
+    sem_init(&n_plein, 0, 0);
+    sem_init(&n_vide, 0, TAILLE_TAMPON);
 
     if (pthread_create(&filsA, NULL, affichage, "AA")) {
         perror("pthread_create");
